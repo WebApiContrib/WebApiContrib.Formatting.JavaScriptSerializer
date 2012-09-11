5667e52c605f5f51480088d00801521cb85ee64f
Ryan Riley <ryan.riley@panesofglass.org>
2012-09-11 10:13:17 -0500

    Update to RTM

--- src/WebApiContrib.Formatting.JavaScriptSerializer/JavaScriptSerializerFormatter.cs
+++ src/WebApiContrib.Formatting.JavaScriptSerializer/JavaScriptSerializerFormatter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Net.Http;
 using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -34,7 +35,7 @@ namespace WebApiContrib.Formatting
             return true;
         }
 
-        public override Task<object> ReadFromStreamAsync(Type type, Stream stream, HttpContentHeaders contentHeaders, IFormatterLogger formatterLogger)
+        public override Task<object> ReadFromStreamAsync(Type type, Stream stream, HttpContent content, IFormatterLogger formatterLogger)
         {
             var tcs = new TaskCompletionSource<object>();
 
@@ -56,7 +57,7 @@ namespace WebApiContrib.Formatting
             return tcs.Task;
         }
 
-        public override Task WriteToStreamAsync(Type type, object value, Stream stream, HttpContentHeaders contentHeaders, TransportContext transportContext)
+        public override Task WriteToStreamAsync(Type type, object value, Stream stream, HttpContent content, TransportContext transportContext)
         {
             var tcs = new TaskCompletionSource<object>();
 
